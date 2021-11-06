5dec8fb738c600808878eeb4fcf2ccc3aec4017a
Pranav Sambyal <singhsambyalpranav@gmail.com>
2021-11-06 22:51:10 +0530

    Corrected getDataWord() to work with list to string  and produce correct tokenized output

--- src/com/pranav/nits/Main.java
+++ src/com/pranav/nits/Main.java
@@ -1,5 +1,6 @@
 package com.pranav.nits;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,53 +8,73 @@ public class Main {
 
     private static void crc() {
         System.out.print("Simulating CRC(CRC8) error detection and correction\nEnter Dataword->");
-        long[] dataWordTokenized=getDataWord(0);
-        for (long x :dataWordTokenized) {
-            System.out.print(x+" ");
-        }
-        System.out.println("\nEnter number of Hops the messsage will travel");
+        String[] dataWordTokenized=getDataWord(0);
+        System.out.println(Arrays.toString(dataWordTokenized) +"\nEnter number of Hops the messsage will travel");
         int hops=getHops();
 
     }
     private static void hammingCode() {
         System.out.print("Simulating 7-Bit Hamming Code error detection and correction\nEnter Dataword->");
-        long dataWord[]=getDataWord(1);
-        System.out.println(dataWord+"\nEnter number of Hops the messsage will travel");
+        String[] dataWordTokenized =getDataWord(1);
+        System.out.println(Arrays.toString(dataWordTokenized) +"\nEnter number of Hops the messsage will travel");
         int hops=getHops();
     }
-    private static long[] getDataWord(int c){
+    private static String[] getDataWord(int c){
         String temp=input.next();
-        switch (c){
-            case 0:
-            {
-                int len;
-                int start=0;
-                if(temp.length()%16!=0){
-                    len=(temp.length()/16)+1;
-                }
-                else {
-                    len = (temp.length() / 16);
-                }
-                long[] ret=new long[len];
-                long j=0;
-                for(int i=0;i<(len*16);i++) {
-                    if(i < temp.length()){
-                        j += temp.charAt(i)-48;
-                    }
-                    else {
-                        j += 0;
-                    }
-                    j *= 10;
-                    if (((i+1) % 16) == 0) {
-                        ret[start] = j;
-                        j = 0;
-                        start++;
-                    }
-                }
-                return ret;
+        if (c == 0) {
+            int len;
+            int start = 0;
+            if (temp.length() % 16 != 0) {
+                len = (temp.length() / 16) + 1;
+            } else {
+                len = (temp.length() / 16);
             }
+            String[] ret = new String[len];
+            StringBuilder j = new StringBuilder();
+            for (int i = 0; i < (len * 16); i++) {
+                if (i < temp.length()) {
+                    j.append(temp.charAt(i));
+                } else {
+                    j.append(0);
+                }
+
+
+                if (((i + 1) % 16) == 0) {
+                    ret[start] = j.toString();
+                    j = new StringBuilder();
+                    start++;
+                }
+            }
+            return ret;
         }
-        return new long[0];
+        else if(c == 1)
+        {
+            int len;
+            int start = 0;
+            if (temp.length() % 4 != 0) {
+                len = (temp.length() / 4) + 1;
+            } else {
+                len = (temp.length() / 4);
+            }
+            String[] ret = new String[len];
+            StringBuilder j = new StringBuilder();
+            for (int i = 0; i < (len * 4); i++) {
+                if (i < temp.length()) {
+                    j.append(temp.charAt(i));
+                } else {
+                    j.append(0);
+                }
+
+
+                if (((i + 1) % 4) == 0) {
+                    ret[start] = j.toString();
+                    j = new StringBuilder();
+                    start++;
+                }
+            }
+            return ret;
+        }
+        return new String[0];
     }
     private static int getHops(){
         int temp=input.nextInt();
